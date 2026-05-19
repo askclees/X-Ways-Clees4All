18ff4b788621446a273f50cdb4930734a0baa5ba
askclees <clees@askclees.com>
2026-05-19 14:58:01 +0100

    utility.cpp/h: fix bugs and apply documentation/code standards
    - Remove unused retVal variable in convertWideToChar
    - Fix copy-paste error in extendBuffer doc (wrongly described as FILETIME converter)
    - Add const to ifFileExists, ifFileExistsW, convertWideToChar, DirExistsW parameters
    - Convert all NaturalDocs blocks (Constant/Section/Function) to Doxygen
    - Remove version strings from doc comments

--- include/utility.h
+++ include/utility.h
@@ -1,12 +1,11 @@
 #ifndef UTILITY_H_INCLUDED
 #define UTILITY_H_INCLUDED
 
-//existence of files functions
-BOOL ifFileExists(char* path);
+BOOL ifFileExists(const char* path);
 BOOL DirExists(LPCTSTR szPath);
-BOOL ifFileExistsW(wchar_t* path);
-BOOL DirExistsW(LPWSTR szPath);
-char* convertWideToChar(wchar_t* wString);
+BOOL ifFileExistsW(const wchar_t* path);
+BOOL DirExistsW(LPCWSTR szPath);
+char* convertWideToChar(const wchar_t* wString);
 INT64 Filetime2INT64(FILETIME &inFT);
 wchar_t* extendBuffer(wchar_t* currBuffer, INT64 currSize, INT64 newSize);
 INT64 FileSize(const char* filePath);
--- src/utility.cpp
+++ src/utility.cpp
@@ -1,112 +1,68 @@
 
 #include <windows.h>
 
-//used for converting filetime to UNIX times
-
-/*Constant: Nano2Seconds
-    Number of nanoseconds in a second
-
-  Constant: EpochDifference
-    Difference between FILETIME and Unix Epoch timestamps
-
-*/
+/** @brief Number of nanoseconds in a second. */
 #define Nano2Seconds 10000000LL
+
+/** @brief Difference between Windows FILETIME epoch and Unix epoch in seconds. */
 #define EpochDifference 11644473600LL
 
-/* Section:
-    Utility functions that are called by various modules within the X-Tension
-*/
+// Utility functions called by various modules within the X-Tension
 
-/*Function: DirExists
-
-   Utility function to determine if directory exists
-
-    Parameters:
-
-        LPCTSTR szPath - Path to directory to be checked. Should be NULL terminated
-
-    Returns:
-        BOOL    -   True if path exists and is a directory, otherwise false
-
-*/
-
+/**
+ * @brief Checks whether a path exists and refers to a directory.
+ *
+ * @param szPath Null-terminated path to check.
+ * @return       TRUE if the path exists and is a directory, FALSE otherwise.
+ */
 BOOL DirExists(LPCTSTR szPath)
 {
     DWORD dwAttrib = GetFileAttributes(szPath);
     return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
 
-/*Function: ifFileExists
-
-   Utility function to determine if file exists
-
-    Parameters:
-
-        LPCTSTR szPath - Path to file to be checked. Should be NULL terminated
-
-    Returns:
-        BOOL    -   True if file exists, otherwise false
-
-*/
-
-BOOL ifFileExists(char* path)
+/**
+ * @brief Checks whether a file exists at the given path.
+ *
+ * @param path Null-terminated path to the file.
+ * @return     TRUE if the file exists, FALSE otherwise.
+ */
+BOOL ifFileExists(const char* path)
 {
     DWORD chkAttributes = GetFileAttributes(path);
     return (chkAttributes != INVALID_FILE_ATTRIBUTES);
 }
 
-/*Function: DirExistsW
-
-   Utility function to determine if directory exists. Wide character version of <DirExists>
-
-    Parameters:
-
-        LPWSTR szPath - Path to directory to be checked. Should be NULL terminated
-
-    Returns:
-        BOOL    -   True if path exists and is a directory, otherwise false
-
-*/
-
-BOOL DirExistsW(LPWSTR szPath)
+/**
+ * @brief Checks whether a path exists and refers to a directory (wide-character version).
+ *
+ * @param szPath Null-terminated wide path to check.
+ * @return       TRUE if the path exists and is a directory, FALSE otherwise.
+ */
+BOOL DirExistsW(LPCWSTR szPath)
 {
     DWORD dwAttrib = GetFileAttributesW(szPath);
     return (dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
 
-/*Function: ifFileExistsW
-
-   Utility function to determine if file exists. Wide character version of <ifFileExists>
-
-    Parameters:
-
-        wchar_t* path   - Path to file to be checked. Should be NULL terminated
-
-    Returns:
-        BOOL    -   True if file exists, otherwise false
-
-*/
-
-BOOL ifFileExistsW(wchar_t* path)
+/**
+ * @brief Checks whether a file exists at the given path (wide-character version).
+ *
+ * @param path Null-terminated wide path to the file.
+ * @return     TRUE if the file exists, FALSE otherwise.
+ */
+BOOL ifFileExistsW(const wchar_t* path)
 {
     DWORD chkAttributes = GetFileAttributesW(path);
     return (chkAttributes != INVALID_FILE_ATTRIBUTES);
 }
 
-
-/*Function: Filetime2Unix
-
-   Utility Function that converts a FILETIME to a Unix Timestamp
-
-    Parameters:
-
-        INT64 fTime - FILETIME to be converted
-
-    Returns:
-        INT64 - Unix timestamp equivalent.
-
-*/
-
+/**
+ * @brief Converts a Windows FILETIME integer to a Unix timestamp.
+ *
+ * @param fTime FILETIME value expressed as a 64-bit integer.
+ * @return      Equivalent Unix timestamp in seconds since the Unix epoch.
+ */
 INT64 Filetime2Unix(INT64 fTime)
 {
     INT64 temp;
@@ -115,74 +71,63 @@ INT64 Filetime2Unix(INT64 fTime)
     return temp;
 }
 
-/*Function: convertWideToChar
-    Utility function to safely convert Wide characters to a UTF-8 string
-    Takes a Wide Character parameter - must be NULL terminated
-    Implemented in version 1.38
-
-    Parameters:
-        wchar_t* wString    -   Wide character string that is to be converted to UTF-8
-
-    Returns:
-        char* of UTF-8 interpretation of Wide Character String
-*/
-
-char* convertWideToChar(wchar_t* wString)
+/**
+ * @brief Converts a wide-character string to a UTF-8 char string.
+ *
+ * The returned buffer is allocated with new[] and must be freed by the caller
+ * using delete[].
+ *
+ * @param wString Null-terminated wide-character string to convert.
+ * @return        Newly allocated null-terminated UTF-8 string.
+ */
+char* convertWideToChar(const wchar_t* wString)
 {
-    int bufferSize = WideCharToMultiByte(CP_UTF8,0,wString, -1, NULL, 0, NULL, NULL );
+    int bufferSize = WideCharToMultiByte(CP_UTF8, 0, wString, -1, NULL, 0, NULL, NULL);
     char* retStr = new char[bufferSize];
-    int retVal = WideCharToMultiByte(CP_UTF8,0,wString, -1, retStr, bufferSize, NULL, NULL );
+    WideCharToMultiByte(CP_UTF8, 0, wString, -1, retStr, bufferSize, NULL, NULL);
     return retStr;
 }
 
-/*Function: Filetime2INT64
-    Utility Function to convert a FILETIME to 64 bit Integer
-
-    Returns:
-        INT64 - value of FILETIME
-
-*/
-
-
+/**
+ * @brief Converts a FILETIME structure to a 64-bit integer.
+ *
+ * @param inFT FILETIME structure to convert.
+ * @return     64-bit integer representation of the FILETIME value.
+ */
 INT64 Filetime2INT64(FILETIME &inFT)
 {
     return ((INT64(inFT.dwHighDateTime)<<32) | (INT64(inFT.dwLowDateTime)));
 }
 
-
-
-/*Function: extendBuffer
-    Utility Function to convert a FILETIME to 64 bit Integer
-
-    Returns:
-        wchar_t* - new buffer of size newSize
-
-*/
+/**
+ * @brief Grows a wide-character buffer, copying existing content into the new allocation.
+ *
+ * The old buffer is freed. The caller takes ownership of the returned buffer and
+ * must free it with delete[].
+ *
+ * @param currBuffer Existing buffer to copy from; freed by this function.
+ * @param currSize   Number of wide characters currently in @p currBuffer.
+ * @param newSize    Number of wide characters to allocate for the new buffer.
+ * @return           Newly allocated buffer of @p newSize wide characters.
+ */
 wchar_t* extendBuffer(wchar_t* currBuffer, INT64 currSize, INT64 newSize)
 {
     wchar_t* retBuffer = new wchar_t[newSize];
-    memcpy(retBuffer,currBuffer,currSize* sizeof(wchar_t));
+    memcpy(retBuffer, currBuffer, currSize * sizeof(wchar_t));
     delete[] currBuffer;
     return retBuffer;
 }
 
-/*Function: FileSize
-    Utility function to return size of a file given its path
-
-    Added in 1.50
-
-    Parameters:
-        const char* filePath    -   Pointer to Null terminated path
-
-    Returns:
-        int64_t -   Size of file
-        -1      -   Error
-*/
-
+/**
+ * @brief Returns the size of a file in bytes.
+ *
+ * @param filePath Null-terminated path to the file.
+ * @return         File size in bytes, or -1 if the attributes could not be retrieved.
+ */
 INT64 FileSize(const char* filePath)
 {
     WIN32_FILE_ATTRIBUTE_DATA fad;
-    if (!GetFileAttributesExA(filePath, GetFileExInfoStandard,&fad))
+    if (!GetFileAttributesExA(filePath, GetFileExInfoStandard, &fad))
         return -1;
     LARGE_INTEGER size;
     size.HighPart = fad.nFileSizeHigh;
